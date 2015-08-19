003c3cf7d5cdf87af27e38d45ea1ac0661f28697
justinzant <justinzant@gmail.com>
2015-08-19 09:52:32 +0200

    Start of the initial project hiearchy

---
+++ app/main.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+/* App Module */
+
+var tempApp = angular.module('templateApp', [
+    'ngRoute',
+    'mainControllers',
+]);
+
+/* Look at URL and fetch the phoneID */
+/* Provide route based on URL & controller */
+tempApp.config(['$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
+      $routeProvider.
+         when('/', {
+            templateUrl: 'views/home/home.html',
+            controller: 'main'
+         })
+          .when('/views/games/:gameId', {
+            templateUrl: 'views/games/games.html',
+            controller: 'games'
+        }).otherwise({
+            redirectTo: '/'
+        });
+
+}]);
---
+++ controllers/maincontrollers.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+
+var tempModule = angular.module('mainControllers', []);
+
+tempModule.controller('main', function ($scope) {
+    $scope.appTitle = "TemplateApp";
+});
+
+
+//Fetch .json lists to retrieve Data for URLs & UL lists
+tempModule.controller('navigation', function ($scope, $http) {
+    $http.get("views/navigation/navigationList.json").success(function (navData) {
+        $scope.navList = navData.Navigation;
+    });
+});
+
+//Fetch .json Template info; file will have to be identical to id of both the routeParam and LinkId
+tempModule.controller('games', ['$scope', '$routeParams', '$http',
+    function ($scope, $routeParams, $http) {
+        $http.get('/views/games/' + $routeParams.gameId + '.json').
+            success(function (gdata) {
+                $scope.gamesData = gdata.Game;
+            }).
+            error(function(edata){
+                $http.get("/views/games/games.json").success(function (gdataFallback) {
+                    $scope.gamesData = gdataFallback.GamesList;
+                });
+            });
+    }]);
+
+
+
